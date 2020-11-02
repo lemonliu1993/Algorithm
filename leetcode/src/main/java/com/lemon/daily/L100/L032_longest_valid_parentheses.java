7899acc2bcad63c9bfec2e10d889e9e7547287fe
lemonliu1993 <2305247091@qq.com>
2020-11-02 23:05:09 +0800

    L032_longest_valid_parentheses

---
+++ leetcode/src/main/java/com/lemon/daily/L100/L032_longest_valid_parentheses.java
@@ -0,0 +1,30 @@
+package com.lemon.daily.L100;
+
+/**
+ * Created by lemoon on 2020/11/2 11:04 PM
+ */
+public class L032_longest_valid_parentheses {
+    public int longestValidParentheses(String s) {
+
+        int result = 0;
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (')' == s.charAt(i)) {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = i > 2 ? dp[i - 2] + 2 : 2;
+                } else {
+                    if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = i - dp[i - 1] - 2 > 0 ? dp[i - 1] + dp[i - dp[i - 1] - 2] + 2 : dp[i - 1] + 2;
+                    }
+                }
+
+                result = Math.max(result, dp[i]);
+            }
+        }
+        return result;
+    }
+
+}

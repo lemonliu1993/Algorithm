035cb3f2147fa588a7ac0cd78485f469941fbfa0
lemonliu1993 <2305247091@qq.com>
2021-01-15 23:55:24 +0800

    L016_3sum_closest

---
+++ leetcode/src/main/java/com/lemon/daily/L100/L016_3sum_closest.java
@@ -0,0 +1,12 @@
+package com.lemon.daily.L100;
+
+/**
+ * Created by lemoon on 2021/1/12 10:33 PM
+ */
+public class L016_3sum_closest {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0]+nums[1]+nums[2];
+        return 0;
+
+    }
+}
